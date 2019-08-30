a5e9a392dfc695cf7543417ecf91ef5b45593d40
fngoo <38164287+fngoo@users.noreply.github.com>
2019-08-31 01:42:32 +0800

    Update getjs.sh

--- getjs.sh
+++ getjs.sh
@@ -5,6 +5,7 @@ echo 'for((i=1;i<=$(cat getjs.txt|wc -l);i+=2)); do file=$(head -$i getjs.txt |t
 echo '#!/bin/bash'>getjs2.sh
 echo 'for((i=2;i<=$(cat getjs.txt|wc -l);i+=2)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L $file>>$output/3_js/$i.js; done'>>getjs2.sh
 echo '#!/bin/bash'>exe_getjs.sh
+echo 'output=$output'>>exe_getjs.sh
 echo 'bash getjs1.sh'>>exe_getjs.sh
 echo 'bash getjs2.sh'>>exe_getjs.sh
 cat exe_getjs.sh | parallel -j 2
