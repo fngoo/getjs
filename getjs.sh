cd9f49859119d23c8e4ae2566995e6dd352cde44
fngoo <38164287+fngoo@users.noreply.github.com>
2019-09-02 20:48:37 +0800

    Update getjs.sh

--- getjs.sh
+++ getjs.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
  
+a=$(cat getjs.txt|wc -l) ; let ac=a/4 ; let dc=a/2 ; let rock=ac*3
+
 echo '#!/bin/bash'>getjs1.sh
-echo 'for((i=1;i<=$(cat getjs.txt|wc -l);i+=2)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L $file>>$output/3_js/$i.js; done'>>getjs1.sh
+echo 'for((i=1;i<$ac;i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L $file>>$output/3_js/$i.js; done'>>getjs1.sh
 echo '#!/bin/bash'>getjs2.sh
-echo 'for((i=2;i<=$(cat getjs.txt|wc -l);i+=2)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L $file>>$output/3_js/$i.js; done'>>getjs2.sh
+echo 'for((i=$ac;i<$dc;i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L $file>>$output/3_js/$i.js; done'>>getjs2.sh
+echo '#!/bin/bash'>getjs3.sh
+echo 'for((i=$dc;i<$rock;i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L $file>>$output/3_js/$i.js; done'>>getjs3.sh
+echo '#!/bin/bash'>getjs4.sh
+echo 'for((i=$rock;i<$a;i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L $file>>$output/3_js/$i.js; done'>>getjs4.sh
 echo '#!/bin/bash'>exe_getjs.sh
 echo 'output=$output'>>exe_getjs.sh
 echo 'bash getjs1.sh'>>exe_getjs.sh
 echo 'bash getjs2.sh'>>exe_getjs.sh
-cat exe_getjs.sh | parallel -j 2
+echo 'bash getjs3.sh'>>exe_getjs.sh
+echo 'bash getjs4.sh'>>exe_getjs.sh
+
+cat exe_getjs.sh | parallel -j 4
