7210a14ed81b8e8ccc3c9d024c3281a5207dbb5f
fngoo <38164287+fngoo@users.noreply.github.com>
2019-09-18 18:31:05 +0800

    Update getjs.sh

--- getjs.sh
+++ getjs.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
- 
+
+x=4 ; input=httprobe.txt ; export x=4 ; export input=httprobe.txt
+length=`wc -l $input|grep -o -P ".*?(?=\ )"`
+#dir_num=$((x*x))
+if [ $length -lt $x ]
+then
+i=1
+a=$(cat getjs.txt|wc -l)
+mkdir $output/3_js
+echo '#!/bin/bash'>getjs1.sh
+echo 'for((i=1;i<$a;i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L $file>>$output/3_js/$i.js; done'>>getjs1.sh
+bash getjs1.sh
+rm getjs1.sh
+
+
+else
+
 a=$(cat getjs.txt|wc -l) ; let ac=a/4 ; let dc=a/2 ; let rock=ac*3
 
 mkdir $output/3_js
@@ -19,3 +35,4 @@ echo 'bash getjs3.sh'>>exe_getjs.sh
 echo 'bash getjs4.sh'>>exe_getjs.sh
 
 cat exe_getjs.sh | parallel --jobs 0 --progress --delay 1 ; rm getjs1.sh ; rm getjs2.sh ; rm getjs3.sh ; rm getjs4.sh ;  rm exe_getjs.sh
+fi
