088ae88e6e3be0333637369a34c60d07aac00d3c
fngoo <38164287+fngoo@users.noreply.github.com>
2019-10-01 12:24:47 +0800

    Update getjs.sh

--- getjs.sh
+++ getjs.sh
@@ -20,13 +20,13 @@ a=$(cat getjs.txt|wc -l) ; ac=$((a/4)) ; dc=$((a/2)) ; rock=$((ac*3))
 
 mkdir $output/3_js
 echo '#!/bin/bash'>getjs1.sh
-echo 'for((i=1;i<$ac;i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_js/$i.js; done'>>getjs1.sh
+echo 'for((i=1;i<'$ac';i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_js/$i.js; done'>>getjs1.sh
 echo '#!/bin/bash'>getjs2.sh
-echo 'for((i='$ac';i<$dc;i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_js/$i.js; done'>>getjs2.sh
+echo 'for((i='$ac';i<'$dc';i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_js/$i.js; done'>>getjs2.sh
 echo '#!/bin/bash'>getjs3.sh
-echo 'for((i='$dc';i<$rock;i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_js/$i.js; done'>>getjs3.sh
+echo 'for((i='$dc';i<'$rock';i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_js/$i.js; done'>>getjs3.sh
 echo '#!/bin/bash'>getjs4.sh
-echo 'for((i='$rock';i<$a;i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_js/$i.js; done'>>getjs4.sh
+echo 'for((i='$rock';i<'$a';i+=1)); do file=$(head -$i getjs.txt |tail -1); output=$output; touch $output/3_js/$i.js; curl -L --speed-time 5 --speed-limit 1 $file>>$output/3_js/$i.js; done'>>getjs4.sh
 echo '#!/bin/bash'>exe_getjs.sh
 echo 'output=$output'>>exe_getjs.sh
 echo 'bash getjs1.sh'>>exe_getjs.sh
