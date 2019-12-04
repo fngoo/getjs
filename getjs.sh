9c1fcbbd717723d18b9b04d80cd4824107677bd5
fngoo <38164287+fngoo@users.noreply.github.com>
2019-12-04 18:09:20 +0800

    Update getjs.sh

--- getjs.sh
+++ getjs.sh
@@ -7,3 +7,4 @@ mkdir $output/3_js
 for((i=1;i<=$a;i+=1)); do file=$(head -$i getjs.txt |tail -1) ; echo "touch $output/3_js/$i.js; curl -k -i -L --speed-time 5 --speed-limit 1 \"$file\" >> $output/3_js/$i.js" >> exe.sh; done
 
 cat exe.sh | parallel --jobs 0 --delay 0.1 --retries 1 ; rm exe.sh
+exit
